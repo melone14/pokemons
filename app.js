59d789b2503506c37f546e5afff26bf9c4b884ba
meloniak14 <w.melone97@gmail.com>
2021-05-24 18:41:33 +0100

    Style pokemon and scrollbar

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log("HELLO");
+const container = document.querySelector("section");
+const button = document.querySelector("button");
+
+let counter = 1;
+
+const addPokemon = () => {
+  if (counter === 900) {
+    button.removeEventListener("click", addPokemon);
+    button.classList.add("disabled");
+  }
+
+  const image = document.createElement("img");
+  image.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${counter}.png`;
+  image.classList.add("pokemon");
+  container.appendChild(image);
+  counter++;
+};
+
+button.addEventListener("click", addPokemon);
